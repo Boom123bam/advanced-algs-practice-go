b3d4a45c646f8c5ab693d3637e0c0df6ca8b846f
Brandon <b.tgchf@gmail.com>
2026-01-13 22:14:08 +0000

    2

--- lab2/lab2.go
+++ lab2/lab2.go
@@ -2,20 +2,61 @@ package main
 
 // Exercise 1: Quicksort
 func quickSort(arr []int) []int {
-	// TODO: Implement Quicksort using Divide and Conquer
-	return arr
+	if len(arr) <= 1 {
+		return arr
+	}
+	pivot := arr[len(arr)/2]
+	left := []int{}
+	right := []int{}
+	for i, num := range arr {
+		if i == len(arr)/2 {
+			continue
+		}
+		if num < pivot {
+			left = append(left, num)
+		} else {
+			right = append(right, num)
+		}
+	}
+	return append(append(quickSort(left), pivot), quickSort(right)...)
 }
 
 // Exercise 2: Find k-th Smallest Element
 func kthSmallest(arr []int, k int) int {
-	// TODO: Implement using pivot-based Divide and Conquer
 	// k is 1-indexed (1 = smallest element)
-	return 0
+	if len(arr) == 1 {
+		return arr[0]
+	}
+	mid := len(arr) / 2
+	pivot := arr[mid]
+	left := []int{}
+	right := []int{}
+	for i, num := range arr {
+		if i == mid {
+			continue
+		}
+		if num < pivot {
+			left = append(left, num)
+		} else {
+			right = append(right, num)
+		}
+	}
+	if k <= len(left) {
+		return kthSmallest(left, k)
+	}
+	if k == len(left)+1 {
+		return pivot
+	}
+	return kthSmallest(right, k-len(left)-1)
 }
 
 // Exercise 3: Find Median
 func findMedian(arr []int) float64 {
-	// TODO: Implement median finding using Divide and Conquer
 	// Hint: Use kthSmallest as inspiration
-	return 0.0
+
+	if len(arr)%2 == 1 {
+		return float64(kthSmallest(arr, len(arr)/2+1))
+	}
+	return float64(kthSmallest(arr, len(arr)/2)+kthSmallest(arr, len(arr)/2+1)) / 2
+
 }
